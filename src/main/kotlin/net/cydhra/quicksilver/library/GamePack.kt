338db235cc3b69a3c01c4c9b8ff01f4f2c9d76fa
Cydhra <ubezl@student.kit.edu>
2020-07-19 02:35:01 +0200

    lazy gamepack parsing and installation routine

--- src/main/kotlin/net/cydhra/quicksilver/library/GamePack.kt
+++ src/main/kotlin/net/cydhra/quicksilver/library/GamePack.kt
@@ -1,8 +1,19 @@
 package net.cydhra.quicksilver.library
 
-class GamePack {
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import net.cydhra.quicksilver.data.pack.GamePackDefinition
+import java.io.File
 
-    fun open() {
+class GamePack(val definitionFile: File) {
 
+    val gamePackDefinition: GamePackDefinition by lazy {
+        Json(JsonConfiguration.Stable).parse(GamePackDefinition.serializer(), definitionFile.readText())
+    }
+
+    fun install() {
+        gamePackDefinition.installation.steps.forEach { step ->
+            Installer.execute(step)
+        }
     }
 }
---
+++ src/main/kotlin/net/cydhra/quicksilver/library/Installer.kt
@@ -0,0 +1,9 @@
+package net.cydhra.quicksilver.library
+
+import net.cydhra.quicksilver.data.pack.InstallationStep
+
+object Installer {
+    fun execute(step: InstallationStep) {
+
+    }
+}
