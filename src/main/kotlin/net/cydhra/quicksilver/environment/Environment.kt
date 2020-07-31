5f805d53cff383b8e827575f1252f7e69967d4eb
Cydhra <ubezl@student.kit.edu>
2020-07-31 23:44:44 +0200

    processes will now be run in background and a future will wait on their exit

--- src/main/kotlin/net/cydhra/quicksilver/environment/Environment.kt
+++ src/main/kotlin/net/cydhra/quicksilver/environment/Environment.kt
@@ -3,11 +3,19 @@ package net.cydhra.quicksilver.environment
 import com.profesorfalken.jpowershell.PowerShell
 import com.profesorfalken.jpowershell.PowerShellResponseHandler
 import java.io.File
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 /**
  * A wrapper for system-dependent calls
  */
 object Environment {
+
+    /**
+     * A cached thread pool to run system tasks and wait for their completion
+     */
+    private val threadPool = Executors.newCachedThreadPool()
 
     /**
      * Start a process using means of the operating system.
@@ -16,25 +24,32 @@ object Environment {
      * @param executable the executable to start a process of
      * @param arguments programm arguments
      * @param elevated whether the process should be started with elevated permissions
+     *
+     * @return a future on the exit code of the process
      */
-    fun startProcess(workingDirectory: File, executable: File, arguments: String, elevated: Boolean) {
+    fun startProcess(workingDirectory: File, executable: File, arguments: String, elevated: Boolean): Future<Int> {
         // TODO operating system dependent stuff
 
-        val powerShell = PowerShell.openSession()
-            .executeCommandAndChain("\$newProcess = new-object System.Diagnostics.Process")
-            .executeCommandAndChain("\$newProcess.StartInfo.FileName = \"${executable.absolutePath}\"")
-            .executeCommandAndChain("\$newProcess.StartInfo.Arguments = \"$arguments\"")
-            .executeCommandAndChain("\$newProcess.StartInfo.WorkingDirectory = \"${workingDirectory.absolutePath}\"")
+        return this.threadPool.submit(Callable {
+            val powerShell = PowerShell.openSession()
+                .executeCommandAndChain("\$newProcess = new-object System.Diagnostics.Process")
+                .executeCommandAndChain("\$newProcess.StartInfo.FileName = \"${executable.absolutePath}\"")
+                .executeCommandAndChain("\$newProcess.StartInfo.Arguments = \"$arguments\"")
+                .executeCommandAndChain("\$newProcess.StartInfo.WorkingDirectory = \"${workingDirectory.absolutePath}\"")
 
-        if (elevated) {
-            powerShell.executeCommandAndChain("\$newProcess.StartInfo.Verb = \"runas\"")
-        }
+            if (elevated) {
+                powerShell.executeCommandAndChain("\$newProcess.StartInfo.Verb = \"runas\"")
+            }
 
-        powerShell
-            .executeCommandAndChain("\$newProcess.Start()",
-                PowerShellResponseHandler { response -> println(response.commandOutput) })
-            .close()
+            var exitCode = 0
+            powerShell
+                .executeCommandAndChain("\$newProcess.Start()",
+                    PowerShellResponseHandler { response -> println(response.commandOutput) })
+                .executeCommandAndChain("\$newProcess.WaitForExit()",
+                    PowerShellResponseHandler { response -> exitCode = response.commandOutput.toInt() })
+                .close()
 
-        // TODO instead of blocking, use future
+            return@Callable exitCode
+        })
     }
 }
