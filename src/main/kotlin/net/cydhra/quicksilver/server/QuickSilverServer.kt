4e8553dccab95f67aa46cd3ec4b13de5dfe7a298
Cydhra <ubezl@student.kit.edu>
2020-08-01 00:29:56 +0200

    added content negotiation

--- src/main/kotlin/net/cydhra/quicksilver/server/QuickSilverServer.kt
+++ src/main/kotlin/net/cydhra/quicksilver/server/QuickSilverServer.kt
@@ -3,14 +3,19 @@ package net.cydhra.quicksilver.server
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.serialization.serialization
 
 @Suppress("unused")
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(ContentNegotiation) {
+        serialization()
+    }
     install(Routing) {
         get("game/install", {})
         get("game/start", startGameInterceptor)
