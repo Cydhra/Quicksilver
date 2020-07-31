8654612a1a92b8d387629b483bcf5c76d9c2fc27
Cydhra <ubezl@student.kit.edu>
2020-08-01 00:25:34 +0200

    moved start game endpoint and prepared new endpoints

--- src/main/kotlin/net/cydhra/quicksilver/server/QuickSilverServer.kt
+++ src/main/kotlin/net/cydhra/quicksilver/server/QuickSilverServer.kt
@@ -1,41 +1,24 @@
 package net.cydhra.quicksilver.server
 
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import net.cydhra.quicksilver.launcher.QuicksilverLauncher
-
-/**
- * path where all game related endpoints are
- */
-const val GAME_PATH = "/game"
-
-/**
- * endpoint to start a game
- */
-const val START_GAME_ENDPOINT = "$GAME_PATH/start"
-
 
 @Suppress("unused")
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
-        get(START_GAME_ENDPOINT) interceptor@{
-            val gameId = call.request.queryParameters["id"]
+        get("game/install", {})
+        get("game/start", startGameInterceptor)
+        get("game/list", {})
+        get("game/info", {})
 
-            if (gameId == null) {
-                call.respond(HttpStatusCode.BadRequest)
-                return@interceptor
-            } else {
-                QuicksilverLauncher.runGame(gameId)
-            }
-        }
+        get("library/list", {})
+        get("library/add", {})
+        get("library/start", {})
     }
 }
---
+++ src/main/kotlin/net/cydhra/quicksilver/server/StartGameEndpoint.kt
@@ -0,0 +1,19 @@
+package net.cydhra.quicksilver.server
+
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.util.pipeline.PipelineInterceptor
+import net.cydhra.quicksilver.launcher.QuicksilverLauncher
+
+val startGameInterceptor: PipelineInterceptor<Unit, ApplicationCall> = interceptor@{
+    val gameId = call.request.queryParameters["id"]
+
+    if (gameId == null) {
+        call.respond(HttpStatusCode.BadRequest)
+        return@interceptor
+    } else {
+        QuicksilverLauncher.runGame(gameId)
+    }
+}
