0beecc9ed5e929f98d371e2be70ae24e42ebc0a4
Cydhra <ubezl@student.kit.edu>
2020-08-01 14:54:36 +0200

    implemented prerequisite disk mounting

--- src/main/kotlin/net/cydhra/quicksilver/data/pack/ExecutionPrerequisite.kt
+++ src/main/kotlin/net/cydhra/quicksilver/data/pack/ExecutionPrerequisite.kt
@@ -1,8 +1,42 @@
 package net.cydhra.quicksilver.data.pack
 
+import com.profesorfalken.jpowershell.PowerShell
 import kotlinx.serialization.Serializable
+import java.io.File
 
 @Serializable
-class ExecutionPrerequisite(
+sealed class ExecutionPrerequisite {
 
-)
+    /**
+     * Mount an iso image as a virtual disk before starting a game. Used for old games relying on physical copies to be
+     * present
+     *
+     * @param imagePath relative path to the disk image
+     */
+    class MountIsoPrerequisite(val imagePath: String) : ExecutionPrerequisite() {
+
+        override fun enable(basePath: File) {
+            val path = File(basePath, imagePath).absolutePath
+            PowerShell.executeSingleCommand("Mount-DiskImage \"$path\"")
+        }
+
+        override fun disable(basePath: File) {
+            val path = File(basePath, imagePath).absolutePath
+            PowerShell.executeSingleCommand("Dismount-DiskImage \"$path\"")
+        }
+    }
+
+    /**
+     * Enable the prerequisite before the execution of a game
+     *
+     * @param basePath the base directory of the game where relative paths are originated
+     */
+    abstract fun enable(basePath: File)
+
+    /**
+     * Disable the prerequisite after a game has finished
+     *
+     * @param basePath the base directory of the game where relative paths are originated
+     */
+    abstract fun disable(basePath: File)
+}
